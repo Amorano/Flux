1a4a41523ffdb5f3b7335794e5cf3353538ccb06
Alexander G. Morano <amorano@gmail.com>
2024-02-18 22:02:04 -0800

    sea creature flow

--- flowfield.py
+++ flowfield.py
@@ -1,4 +1,5 @@
 
+from functools import cache
 import dearpygui.dearpygui as dpg
 from opensimplex import random_seed, noise3array
 from particle import Particle
@@ -6,14 +7,17 @@ import numpy as np
 from PIL import Image
 from util import timeit
 
+from concurrent.futures import ThreadPoolExecutor
+
 random_seed()
 
 TAU = np.pi * 2
 
-_scale = 45
-_cols = 10
-_rows = 10
-particles_total = 2000
+_scale = 60
+_cols = 16
+_rows = 16
+particles = []
+particles_total = 4000
 x_coords, y_coords = np.meshgrid(np.linspace(0, 1, _cols), np.linspace(0, 1, _rows))
 
 _width = _cols * _scale
@@ -26,19 +30,47 @@ dpg.create_context()
 dpg.create_viewport(title='Custom Title', width=_width, height=_height, resizable=False)
 dpg.setup_dearpygui()
 
+DEGREE_POINTS = 360
+DEGREE_VALUES = np.linspace(0, TAU, DEGREE_POINTS)
+table_sin = np.sin(DEGREE_VALUES)
+table_cos = np.cos(DEGREE_VALUES)
+
 def recalc_particles(vector_field: list, vector_field_z: float, zt: float) -> tuple[list, float]:
     if z - vector_field_z >= 0.01:
         vector_field = _flowfield(zt)
         vector_field_z = zt
 
-    force = [(np.cos(vector_field[int(particle.pos[1] // _scale)][int((particle.pos[0] // _scale) % _cols)]),
-            np.sin(vector_field[int(particle.pos[1] // _scale)][int((particle.pos[0] // _scale) % _cols)]))
-            for particle in particles]
+    @cache
+    def func_cos(val:float, round:int=6) -> float:
+        #val = table_cos[np.abs(DEGREE_VALUES - val).argmin()]
+        val = np.cos(val)
+        return np.round(val, round)
 
-    for idx, particle in enumerate(particles):
-        particle.apply_force(force[idx])
-        particle.update()
-        particle.warp_around_edges(_width, _height)
+    @cache
+    def func_sin(val:float, round:int=6) -> float:
+        # val = table_sin[np.abs(DEGREE_VALUES - val).argmin()]
+        val = np.sin(val)
+        return np.round(val, round)
+
+    def calculate_force(p:Particle, round:int=4) -> float:
+        xy = vector_field[int(p.pos[1] // _scale)][int((p.pos[0] // _scale) % _cols)]
+        c = func_cos(xy, round)
+        # np.round(np.cos(xy), round)
+        s = func_sin(xy, round)
+        #np.round(np.sin(xy), round)
+        return (c, s)
+
+    # Function to process particle in a thread
+    def process_particle(p) -> None:
+        p.apply_force(calculate_force(p))
+        p.update()
+        p.warp_around_edges(_width, _height)
+
+    for p in particles:
+        process_particle(p)
+
+    # print(func_sin.cache_info())
+
     return vector_field, vector_field_z
 
 z = 0
@@ -57,7 +89,7 @@ def _handle_frame_buffer(sender, buffer) -> None:
                     dpg.add_raw_texture(width=width, height=height, default_value=buffer, format=dpg.mvFormat_Float_rgba, tag="prev_frame")
                 dpg.add_image('prev_frame', parent='flowfield', pos=(0,0))
                 # Adding a dimmer - once and for good
-                _background(opacity=10)
+                _background(opacity=30)
                 #with open("alex.txt", "w", encoding="utf-8") as f:
                 #    f.write(buffer)
 
@@ -66,7 +98,7 @@ def _handle_frame_buffer(sender, buffer) -> None:
 
             global flowfield, flowfield_z, z
             flowfield, flowfield_z = recalc_particles(flowfield, flowfield_z, z)
-            z += np.random.random() * 0.02
+            z += 10 # * np.random.random()
 
             # Run the next update as soon as we can - something needs an extra frame
             # to render correctly; might be the texture.  That's why we skip a frame.
